a90d87a4e84773eae14904df9bfb1760657e70a0
Ryan Macklin <ryanmacklin@gmail.com>
2022-03-17 02:06:22 +0000

    Adding app back in

---
+++ src/routes/Status.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let text;
+	//export let style;
+	let current = "";
+	
+	$:{
+//		if (text != undefined && current != text) current = text;
+		if (text != undefined && current != text) current = text;
+	}
+	
+</script>
+
+<p>
+	{current}
+</p>
+
+<style>
+	p {
+	   color: white; /* just to keep the warning off */
+	}
+</style>
