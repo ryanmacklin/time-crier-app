981e30e364e480aca1d014d952780f433ed35288
Ryan Macklin <ryanmacklin@gmail.com>
2022-03-19 05:41:11 +0000

    Starting some error display framework

---
+++ src/routes/Error.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let type = "alert"; // alert or warning
+    export let message = "";
+</script>
+{#if (message)}
+<div>
+{type}: {message}
+</div>
+{/if}
+
+<style>
+    
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte';
     import Clock from './Clock.svelte';
     import Notifications from './Notifications.svelte';
+    import Error from './Error.svelte';
 
     /**** GET CONFIG ****/
     let config = freshConfig();
@@ -12,6 +13,8 @@
     let configMaxFreshness = 300; // 5 minutes
     let configMalloadCount = 0;
     let newconfig;
+
+    let errorMessage = "";
 
     function freshConfig() { // a blank config file with just white as the default clock color
         return {
@@ -56,12 +59,17 @@
                 configMalloadCount = 0;
                 configLoading = false;
                 console.log("config file updated");
+            } else {
+                console.log("no updated needed");
             }
         })
         .catch(reason => { // TODO: better UX for when error happens
-            console.log(reason.message); // just console.logging the error for now
-            configFreshness = 15; // try again in 15 seconds
             configMalloadCount++;
+            let m = reason.message;
+            m = "Config file: " + reason.message + " (" + configMalloadCount + " failed load)";
+            console.error(m);
+            errorMessage = m;
+            configFreshness = 15; // try again in 15 seconds
             configLoading = false;
         });
     }
@@ -70,25 +78,22 @@
         let res = [];
         con.forEach(i => {
             // get times
-            // TODO throw error if either time isn't proper military time, in miltime2minutes()
-            //let tag = i.tag; // not bothering with tags right now, even though they're in the config as placeholders
+            let tag = i.tag || ""; // not bothering with tags right now, even though they're in the config as placeholders, but they're useful in debugging
+            //console.log(tag);
+
             let startTime = miltime2minutes(i.startTime);
             let endTime = miltime2minutes(i.endTime);
             if (endTime < startTime) endTime += 1440; // to handle time spanning midnight
-            // start with colors before reading them
-            // TODO throw error if either color is invalid, in getColorComponenets()
-            // there's either just one color for the time, or a color gradient
 
             if (i.color) {
                 let color = getColorComponenets(i.color); // just to check color
                 color = i.color; // if it got here, the color's okay
                 for (let i = startTime; i <= endTime; i++) {
-                    res[i % 1440] = getColor(color, newconfig.colorPresets);
+                    res[i % 1440] = getColor(color, newconfig);
                 }
             } else if (i.startColor && i.endColor) {
-                let startColor = getColorComponenets(getColor(i.startColor, newconfig.colorPresets));
-                let endColor = getColorComponenets(getColor(i.endColor, newconfig.colorPresets));
-                // Math.round(startColor + ( (endColor - startColor) * ((i - startTime) / (endTime - startTime)) ));
+                let startColor = getColorComponenets(getColor(i.startColor, newconfig));
+                let endColor = getColorComponenets(getColor(i.endColor, newconfig));
                 for (let i = startTime; i <= endTime; i++) {
                     let r = Math.round(startColor.r + ( (endColor.r - startColor.r) * ((i - startTime) / (endTime - startTime)) ));
                     let g = Math.round(startColor.g + ( (endColor.g - startColor.g) * ((i - startTime) / (endTime - startTime)) ));
@@ -100,6 +105,7 @@
                     res[i % 1440] = s.toUpperCase();
                 }
             } else {
+                throw "Invalid color schedule entry for tag \"" + tag + "\"";
                 // error!
             }
         });
@@ -107,40 +113,48 @@
         return res;
     }
 
-    function getColor(x, col = null) {
+    function getColor(x, col = config) {
         // TODO handle error!
         if (x.substring(0,1) == "%") {
             x = x.substring(1);
             if (x == "default") {
-                return config.clockColors.default;
-            } else if (col) {
-                return col[x];
+                return col.clockColors.default;
             } else {
-                return config.colorPresets[x];
+                try {
+                    return col.colorPresets[x];
+                }
+                catch {
+                    throw '"' + x + "\" isn't a listed preset color";
+                }
             }
         }
         return x; // doesn't need transation
     }
 
-    function getColorComponenets(c) {
-        // right not this assumes 6-digit hex code (with or without # sign); low-pri todo is maybe not limit to this
-        // TODO throw error if either color is invalid
+    function getColorComponenets(x) {
+        // this assumes 6-digit hex code (with or without # sign)
         let r, g, b;
-
+        let c = x;
         if (c.substring(0,1) == "#") c = c.substring(1);
         r = Number.parseInt(c.substring(0,2), 16);
-        if (Number.isNaN(r)) {} // error!
+        if (Number.isNaN(r)) {
+            console.log("---" + x);
+        }
+        if (Number.isNaN(r)) {} //throw '"' + x + "\" isn't a valid color code";} // error!
         g = Number.parseInt(c.substring(2,4), 16);
-        if (Number.isNaN(g)) {} // error!
+        if (Number.isNaN(g)) {} //throw '"' + x + "\" isn't a valid color code";} // error!
         b = Number.parseInt(c.substring(4,6), 16);
-        if (Number.isNaN(b)) {} // error!
-
+        if (Number.isNaN(b)) {} //throw '"' + x + "\" isn't a valid color code";} // error!
         return {"r": r, "g": g, "b": b};
     }
 
     function miltime2minutes(t) {
-            // TODO throw error if either time isn't proper military time
-            return (Math.trunc(t / 100) * 60) + (t % 100);
+        if (!Number.isInteger(t)) {throw '"' + t.toString() + "\" isn't a valid military time";} // error!
+        let hours = Math.trunc(t / 100);
+        let minutes = t % 100;
+        if (hours > 23 || hours < 0) {throw '"' + t.toString() + "\" isn't a valid military time";} // error!
+        if (minutes > 59 || minutes < 0) {throw '"' + t.toString() + "\" isn't a valid military time";} // error!
+        return (Math.trunc(t / 100) * 60) + (t % 100);
     }
 
     // config file initial & periodic loading
@@ -231,4 +245,4 @@
 
   <Notifications primary="Sleep is respecting yourself" secondary="Can you do 5 minutes of exercise soon?"></Notifications>
 
-  
+  <Error />
